9092e36cf7b2b1516c1d5b426e4e34c2812fcc27
idastormankerstjerne <ida.s.ankerstjerne@gmail.com>
2025-11-17 18:41:47 +0100

    Activies In Class

---
+++ DigitSumModulo/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter an integer: ");
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (number > 0)
+{
+    int digit = number % 10;   
+    sum = sum + digit;  
+    number = number / 10;  
+}
+
+Console.WriteLine("Sum: " + sum);
